85f60a62085acd4fee1c6410becbf3f14a7a648c
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-12-20 14:43:48 -0500

    12/20

---
+++ OrcMathGUI/src/myStuff/CatalogScreen.java
@@ -0,0 +1,47 @@
+package myStuff;
+
+import java.util.List;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.TextArea;
+import guiTeacher.components.TextField;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class CatalogScreen extends FullFunctionScreen{
+
+	private TextField mainField;
+	private Button add;
+	private TextArea area;
+
+	public CatalogScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		mainField = new TextField(40,40,200,30, "Text goes here");
+		viewObjects.add(mainField);
+
+		mainField = new TextField(40,40,200,30, "You go here");
+		viewObjects.add(mainField);
+
+		add = new Button(100,100,100,100, "BigButton", new Action() {
+			
+			@Override
+			public void act() {
+				// TODO Auto-generated method stub
+				mainField.setText("You clicked the button");
+				
+			}
+		});
+		viewObjects.add(add);
+
+		area = new TextArea(640,30,200,20, "This is a textarea");
+		viewObjects.add(area);
+	}
+
+
+}
