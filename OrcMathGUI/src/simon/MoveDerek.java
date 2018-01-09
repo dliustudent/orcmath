9569fe9d5499f71754cf64f456298d552529fad6
dliustudent <dliustudent@gmail.com>
2018-01-09 12:47:44 -0500

    Update MoveDerek.java

--- OrcMathGUI/src/simon/MoveDerek.java
+++ OrcMathGUI/src/simon/MoveDerek.java
@@ -2,10 +2,14 @@ package simon;
 
 public class MoveDerek implements MoveInterfaceDerek{
 
+	ButtonInterfaceDerek button;
+	
+	public MoveDerek(ButtonInterfaceDerek button) {
+		this.button = button;
+	}
 	@Override
 	public ButtonInterfaceDerek getButton() {
-		// TODO Auto-generated method stub
-		return null;
+		return button;
 	}
 
 }
